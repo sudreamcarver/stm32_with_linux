224d993a1e0a8885e171f623c52a87982e282a10
sudreamcarver <lt72496@outlook.com>
2025-01-05 17:03:44 +0800

    .

--- stm_leds/source/main.c
+++ stm_leds/source/main.c
@@ -2,28 +2,28 @@
 #include "stm32f10x.h" // Device header
 
 int main(void) {
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
 
   GPIO_InitTypeDef GPIO_InitStructure;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-  GPIO_Init(GPIOA, &GPIO_InitStructure);
+  GPIO_Init(GPIOC, &GPIO_InitStructure);
 
   while (1) {
-    GPIO_ResetBits(GPIOA, GPIO_Pin_0);
+    GPIO_ResetBits(GPIOC, GPIO_Pin_13);
     Delay_ms(500);
-    GPIO_SetBits(GPIOA, GPIO_Pin_0);
-    Delay_ms(500);
-
-    GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_RESET);
-    Delay_ms(500);
-    GPIO_WriteBit(GPIOA, GPIO_Pin_0, Bit_SET);
+    GPIO_SetBits(GPIOC, GPIO_Pin_13);
     Delay_ms(500);
 
-    GPIO_WriteBit(GPIOA, GPIO_Pin_0, (BitAction)0);
+    GPIO_WriteBit(GPIOC, GPIO_Pin_13, Bit_RESET);
     Delay_ms(500);
-    GPIO_WriteBit(GPIOA, GPIO_Pin_0, (BitAction)1);
+    GPIO_WriteBit(GPIOC, GPIO_Pin_13, Bit_SET);
+    Delay_ms(500);
+
+    GPIO_WriteBit(GPIOC, GPIO_Pin_13, (BitAction)0);
+    Delay_ms(500);
+    GPIO_WriteBit(GPIOC, GPIO_Pin_13, (BitAction)1);
     Delay_ms(500);
   }
 }
